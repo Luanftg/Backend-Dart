f187e65dbd3e02ee1f479d66e0740e70cdd1a588
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-08-19 21:54:00 -0300

    criando rotas get com Mime Types

--- bin/server_handler.dart
+++ bin/server_handler.dart
@@ -9,6 +9,34 @@ class ServeHandler {
       return Response(200, body: 'Primeira rota');
     });
 
+    // http://localhost:8080/ola/mundo
+    /*router.get('/ola/mundo/<usuario>', (Request request, String usuario) {
+      return Response.ok("Ola Mundo $usuario");
+    });*/
+
+    // http://localhost:8080/query?nome=Luan
+    router.get('/query', (Request req) {
+      String? nome = req.url.queryParameters['nome'];
+      return Response.ok('Query é: $nome',
+          headers: {"Content-Type": "text/plain"});
+    });
+
+    // http://localhost:8080/tmd/home
+    router.get('/tmd/home', (Request req) => Response.ok('''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teatro Marcelo Denny</title>
+</head>
+<body>
+    <h1>Teatro Marcelo Denny</h1>
+</body>
+</html>
+''', headers: {"Content-Type": "text/html"}));
+
     return router;
   }
 }
